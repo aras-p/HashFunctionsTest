f9c7186c56ea5fa0cbfd73b17010743228f5480b
Aras Pranckevicius <aras@unity3d.com>
2019-06-07 10:29:10 +0300

    XXH3: manual tweak to make SSE2 code path kick in on MSVC

--- HashFunctions/xxh3.h
+++ HashFunctions/xxh3.h
@@ -92,7 +92,7 @@
 #ifndef XXH_VECTOR    /* can be defined on command line */
 #  if defined(__AVX2__)
 #    define XXH_VECTOR XXH_AVX2
-#  elif defined(__SSE2__)
+#  elif defined(__SSE2__) || defined(_M_AMD64) || defined(_M_X64)
 #    define XXH_VECTOR XXH_SSE2
 /* msvc support maybe later */
 #  elif defined(__GNUC__) \
