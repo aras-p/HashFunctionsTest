255ac51d7d7142f4977ad5bbc7459c72a50ffed8
Aras Pranckevicius <aras@unity3d.com>
2019-07-26 08:07:06 +0300

    Update XXH3 to 0.7.1-ish version from commit f012d4e on 2019 July 26

--- compile_emscripten.sh
+++ compile_emscripten.sh
@@ -1,4 +1,5 @@
 #/usr/bin/bash
 # note: needs 'source ~/proj/other/emsdk/emsdk_env.sh'
+# note: then run a http server, e.g. python -m SimpleHTTPServer, and go to 0.0.0.0:8000
 # note: set privacy.reduceTimerPrecision to false in about:config on Firefox
 emcc -O2 main.cpp HashFunctions/*.cpp HashFunctions/*.c HashFunctions/*.cc HashFunctions/t1ha_src/*.c -o emscripten.html -s TOTAL_MEMORY=268435456 --preload-file TestData/test-words.txt --preload-file TestData/test-filenames.txt --preload-file TestData/test-binary.bin -s WASM=1
